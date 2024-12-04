f6aea0046e90a6a7b1ec9514ee1f30b6bd022729
Rahmad Alif <63047726+alifarsum@users.noreply.github.com>
2024-12-04 16:55:23 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('my-pemasukanpengeluaran').then((cache) => {
             return cache.addAll([
-                'https://alifarsum.github.io/pemasukanpengeluaran.github.io/index.html',
+                '/pemasukanpengeluaran.github.io/index.html',
                 // Tambahkan file lainnya yang ingin dicache
             ]);
         })
