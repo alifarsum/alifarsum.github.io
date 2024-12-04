2c7bfd368f19d140b832f29c343378e42da82ff8
Rahmad Alif <63047726+alifarsum@users.noreply.github.com>
2024-12-04 16:38:37 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-pemasukanpengeluaran').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                // Tambahkan file lainnya yang ingin dicache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['my-pemasukanpengeluaran '];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
